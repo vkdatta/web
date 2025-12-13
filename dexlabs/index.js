bb850321b6350c864d14b2c67073bcb9ecacc595
vkdatta <134008395+vkdatta@users.noreply.github.com>
2025-12-14 03:14:07 +0530

    Update index.js

--- dexlabs/index.js
+++ dexlabs/index.js
@@ -1,1 +1,14 @@
+// note app
 
+import "./app/noteapp/noteappjs/prism.js";
+import "./app/noteapp/noteappjs/undo.js";
+import "./app/noteapp/noteappjs/sidebar2.js";
+import "./app/noteapp/noteappjs/sidebar2/formatting.js";
+import "./app/noteapp/noteappjs/sidebar2/operations.js";
+import "./app/noteapp/noteappjs/sidebar2/code.js";
+import "./app/noteapp/noteappjs/sidebar2/document.js";
+import "./app/noteapp/noteappjs/modal.js";
+
+// diff app
+
+import "./app/diffapp/diffappjs/diffapp.js";
