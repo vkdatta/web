193aaa8f90c89741fe0a9cc4b8beba27a575ddbb
vkdatta <134008395+vkdatta@users.noreply.github.com>
2025-12-14 07:48:17 +0530

    Update index.js

--- dexlabs/index.js
+++ dexlabs/index.js
@@ -1,14 +1,14 @@
 // note app
 
-import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/app/noteapp/noteappjs/prism.js";
-import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/app/noteapp/noteappjs/undo.js";
-import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/app/noteapp/noteappjs/sidebar2.js";
-import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/app/noteapp/noteappjs/sidebar2/formatting.js";
-import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/app/noteapp/noteappjs/sidebar2/operations.js";
-import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/app/noteapp/noteappjs/sidebar2/code.js";
-import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/app/noteapp/noteappjs/sidebar2/document.js";
-import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/app/noteapp/noteappjs/modal.js";
+import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/dexlabs/app/noteapp/noteappjs/prism.js";
+import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/dexlabs/app/noteapp/noteappjs/undo.js";
+import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/dexlabs/app/noteapp/noteappjs/sidebar2.js";
+import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/dexlabs/app/noteapp/noteappjs/sidebar2/formatting.js";
+import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/dexlabs/app/noteapp/noteappjs/sidebar2/operations.js";
+import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/dexlabs/app/noteapp/noteappjs/sidebar2/code.js";
+import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/dexlabs/app/noteapp/noteappjs/sidebar2/document.js";
+import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/dexlabs/app/noteapp/noteappjs/modal.js";
 
 // diff app
 
-import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/app/diffapp/diffappjs/diffapp.js";
+import "https://cdn.jsdelivr.net/gh/vkdatta/web@main/dexlabs/app/diffapp/diffappjs/diffapp.js";
